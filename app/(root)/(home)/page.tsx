7c0183c072deada1f4741a60fdfcc2d5d62b3d69
Bohdan-Dubravin <dubravin111@gmail.com>
2023-11-04 19:48:51 +0200

    add auth and middleware

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { UserButton } from "@clerk/nextjs";
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      home
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default Home;
